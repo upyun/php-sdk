fc0e8b371baae0d478f8a95361f814a64361fc8c
jianhua.wang <jianhua.wang@upai.com>
2014-11-06 12:01:57 +0800

    添加测试用例

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+require_once './tests/config.php';
+require_once './upyun.class.php';
+
+define('USER_NAME', $config['user_name']);
+define('PWD', $config['pwd']);
+define('BUCKET', $config['bucket']);
+
+define('PIC_PATH' , $config['picture_path']);
---
+++ tests/config.php
@@ -0,0 +1,7 @@
+<?php
+$config =  array(
+		'user_name' => 'tester',
+		'pwd' => 'grjxv2mxELR3',
+		'bucket' => 'sdkimg',
+		'picture_path' => './sample.jpeg'
+	);
