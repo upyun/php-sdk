4529c3361d74486b9e0a57db89cb4c0fd6039482
sabakugaara <sabakugaara@users.noreply.github.com>
2017-02-08 09:59:12 +0800

    fix 客户端上传示例 bug
    签名已经更新为新的算法导致

--- examples/client-upload/policy.php
+++ examples/client-upload/policy.php
@@ -10,6 +10,7 @@ $config->setFormApiKey('Mv83tlocuzkmfKKUFbz2s04FzTw=');
 $data['save-key'] = $_GET['save_path'];
 $data['expiration'] = time() + 120;
 $policy = Signature::getFormSignature($config, $data);
+$policy['authorization'] = 'UPYUN ' . USER_NAME . ':' . $policy['signature'];
 echo json_encode($policy);
 
 
