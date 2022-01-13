a5c513af6abb4a3fa659aebe2447ea2d74e7c2e5
xiangkeguo <xiangkeguo@gmail.com>
2022-01-13 16:52:44 +0800

    up

--- src/Upyun/Upyun.php
+++ src/Upyun/Upyun.php
@@ -154,7 +154,7 @@ class Upyun
 
         if (! isset($params['x-upyun-list-iter'])) {
             if (is_resource($saveHandler)) {
-                Psr7\copy_to_stream($response->getBody(), Psr7\stream_for($saveHandler));
+                Psr7\copy_to_stream($response->getBody(), \GuzzleHttp\Psr7\Utils::streamFor($saveHandler));
                 return true;
             } else {
                 return $response->getBody()->getContents();
